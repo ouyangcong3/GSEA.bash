d1b3178f68276a126aac40ba507da8886d65545b
ouyangcong3 <31554882+ouyangcong3@users.noreply.github.com>
2018-11-21 15:58:31 +0800

    1.DEG2
    Input is count

---
+++ DESeq2.r
@@ -0,0 +1,21 @@
+DESeq2diff<-function(Data,Outputname,Connum,Treatnum){
+require(DESeq2)
+require(limma)
+rt=read.table(Data,sep="\t",header=T,check.names=F)
+rt=as.matrix(rt)
+rownames(rt)=rt[,1]
+exp=rt[,-1]
+dimnames=list(rownames(exp),colnames(exp))
+data=matrix(as.numeric(as.matrix(exp)),nrow=nrow(exp),dimnames=dimnames)
+countData<-round(avereps(data),0)
+condition <- factor(c(rep("con",Connum),rep("treat",Treatnum)))
+dds <- DESeqDataSetFromMatrix(countData, DataFrame(condition), design= ~ condition )
+dds <- DESeq(dds)
+res <- results(dds)
+resdata <- merge(as.data.frame(res), as.data.frame(counts(dds, normalized=TRUE)),by="row.names",sort=FALSE)
+write.table(resdata[,1:7],file = paste(Outputname,"_diff.txt",sep=""),sep="\t",quote=F,row.names=F)
+write.table(resdata[,-c(2:7)],file = paste(Outputname,"_normalize.txt",sep=""),sep="\t",quote=F,row.names=F)
+}
+
+DESeq2diff("siPHLDA1_counts.txt","siPHLDA1",2,2)
+DESeq2diff("mRNA.symbol.txt","TCGA_CRC",51,647)
